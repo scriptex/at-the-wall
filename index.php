4546ad1b68f566b73a657af414b314537e0406e8
Atanas Atanasov <scriptex.bg@gmail.com>
2020-04-30 11:14:45 +0300

    Rebuild the app

--- index.php
+++ index.php
@@ -47,27 +47,27 @@ function print_part($name, $count) {
 	For more info about the favicons and meta tags below see
 	https://github.com/audreyr/favicon-cheat-sheet
 	-->
-	<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicon/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicon/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicon/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicon/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicon/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="assets/images/favicon/favicon-196x196.png" sizes="196x196" />
-	<link rel="icon" type="image/png" href="assets/images/favicon/favicon-96x96.png" sizes="96x96" />
-	<link rel="icon" type="image/png" href="assets/images/favicon/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="assets/images/favicon/favicon-16x16.png" sizes="16x16" />
-	<link rel="icon" type="image/png" href="assets/images/favicon/favicon-128.png" sizes="128x128" />
-	<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="favicons/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="favicons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="favicons/favicon-128.png" sizes="128x128" />
+	<link rel="shortcut icon" type="image/x-icon" href="favicons/favicon.ico" />
 	<meta name="application-name" content="At the Wall"/>
 	<meta name="msapplication-TileColor" content="#000000" />
-	<meta name="msapplication-TileImage" content="assets/images/favicon/mstile-144x144.png" />
-	<meta name="msapplication-square70x70logo" content="assets/images/favicon/mstile-70x70.png" />
-	<meta name="msapplication-square150x150logo" content="assets/images/favicon/mstile-150x150.png" />
-	<meta name="msapplication-wide310x150logo" content="assets/images/favicon/mstile-310x150.png" />
-	<meta name="msapplication-square310x310logo" content="assets/images/favicon/mstile-310x310.png" />
+	<meta name="msapplication-TileImage" content="favicons/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="favicons/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="favicons/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="favicons/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="favicons/mstile-310x310.png" />
 	<meta name="msapplication-config" content="config.xml" />
 	<meta name="theme-color" content="#000000" />
 
